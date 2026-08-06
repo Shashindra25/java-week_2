066e2dc1b7e58b770fa7c02136784751aa6b072c
L.Shashindra <lagishettyshashindra@gmail.com>
2026-08-06 11:13:48 +0530

    Updated the Main class.

--- Day_13/Main.java
+++ Day_13/Main.java
@@ -1,3 +1,13 @@
+/*
+ Create a custom exception class "InsufficientBalanceException" that extends RuntimeException or Exception class and override getMessage method.
+   -Create a class "BankingOperations" that extends InsufficientBalanceException class.
+    -Create a method "withdraw" that throws InsufficientBalanceException and takes a double value "currentBalance", an int value "withdrawAmount" as parameters and returns a double value.
+	 -Now throw the InsufficientBalanceException by creating an object of the class, if the currentBalance < withdrawAmount.
+	 -Or else perform withdraw operation by removing withdrawAmount from currentBalance.
+	 -Then return the currentBalance;
+   -Create a class "Main" and create the object of banking operations class and perform withdraw operation.
+    -Also use try catch blocks to handle the exception that comes from the withdraw operation.
+*/
 package com.company.project.module.main;
 
 import com.company.project.module.banking.BankingOperations;
